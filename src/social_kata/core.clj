74c09e4f97f45e9f43a0612ebcd9b3b068e32455
Chris Howe-Jones <chris.howejones@gmail.com>
2015-06-08 20:58:31 +0100

    Added subscriptions and feeds

--- src/social_kata/core.clj
+++ src/social_kata/core.clj
@@ -6,8 +6,15 @@
 
 (defn view
   [state user]
-  (get state user []))
+  (get-in state [user :timeline] []))
 
 (defn subscribe
   [state user subscription]
   (update-in state [user :subscriptions] conj subscription))
+
+(defn feed
+  [state user]
+  (let [subs (get-in state [user :subscriptions])]
+    (for [author subs
+          message (view state author)]
+      {:author author :message message})))
--- test/social_kata/core_test.clj
+++ test/social_kata/core_test.clj
@@ -4,13 +4,13 @@
 
 (deftest a-test
   (testing "Alice can publish to a personal timeline"
-    (is (= {"Alice" ["Hello world."]} 
+    (is (= {"Alice" ["Hello world."]}
            (publish {} "Alice" "Hello world.")))))
 
 (deftest reading-test
   (testing "Bob can view Alice's timeline"
     (let [state-a {}
-          state-b {"Alice" ["A message"]}]
+          state-b {"Alice" {:timeline ["A message"]}}]
       (is (= []
              (view state-a "Alice")))
 
@@ -19,13 +19,25 @@
 
 (deftest subscribe-test
   (let [state {"Charlie" {:timeline [] :subscriptions []}}
-        state-a {"Charlie" {:timeline [] :subscriptions []}
-               "Alice" {:timeline ["A message"] :subscriptions []}}]
+        state-a {"Charlie" {:timeline [] :subscriptions ["Alice"]}
+                 "Alice" {:timeline ["A message"] :subscriptions []}}
+        state-b {"Charlie" {:timeline [] :subscriptions ["Alice" "Bob"]}
+                 "Alice" {:timeline ["A message"] :subscriptions []}
+                 "Bob" {:timeline ["Bob's msg1" "Bob's msg2"]}}]
     (testing "Charlie can subscribe to Alice's timeline"
       (is (= (update-in state ["Charlie" :subscriptions] conj "Alice")
              (subscribe state "Charlie" "Alice"))))
     (testing "Charlie sees alice's timeline"
       (is (= []
              (feed state "Charlie")))
-      (is (= []
-             (feed state-a "Charlie"))))))
+      (is (= [{:author "Alice" :message "A message"}]
+             (feed state-a "Charlie"))))
+    (testing "Charlie can subscribe to Alice and Bob's timeline"
+      (is (= (assoc-in state ["Charlie" :subscriptions] ["Alice" "Bob"])
+             (-> state
+                 (subscribe "Charlie" "Alice")
+                 (subscribe "Charlie" "Bob"))))
+      (is (= [{:author "Alice" :message "A message"}
+              {:author "Bob" :message "Bob's msg1"}
+              {:author "Bob" :message "Bob's msg2"}]
+             (feed state-b "Charlie"))))))
